fd6b17c2dcc46b6b3f144c31aad1ecbd787c50c7
Nawfal <nawfal.cherkaoui614@gmail.com>
2024-11-05 18:39:46 -0500

    Added comment

--- main.c
+++ main.c
@@ -9,7 +9,7 @@
 // sem_t mutex;
 // volatile int counter = 0;
 
-// Function to read file
+// Function to read file.
 pthread_t *readFile(FILE *outputFile)
 {
     FILE *inputFile = fopen("commands.txt", "r");
